7eb0b5194056ee4376b58850d3348f63c51526a7
dr-ishigami-senku <nu522696@gmail.com>
2023-02-27 23:07:57 +0800

    media query for navbar only

--- assets/js/navbar-scrolled.js
+++ assets/js/navbar-scrolled.js
@@ -4,3 +4,11 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+//When any link in navbar is clicked
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navcol-1')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
